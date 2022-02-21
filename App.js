a2515547a65697face3b873299e0af0678f9b915
Jakub Mares <jakub.mares@heidelbergcement.com>
2022-02-21 21:44:18 +0100

    add logic to fetch fonts

--- App.js
+++ App.js
@@ -1,6 +1,29 @@
+import React, { useState } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import * as Font from 'expo-font';
+import AppLoading from 'expo-app-loading';
+
+const fetchFonts = () => {
+  return Font.loadAsync({
+    'open-sans': require('./assets/fonts/OpenSans-Regular.ttf'),
+    'open-sans-bold': require('./assets/fonts/OpenSans-Bold.ttf'),
+  });
+};
 
 export default function App() {
+  const [fontLoaded, setFontLoaded] = useState(false);
+
+  // if font is not loaded, show loading screen and wait till the font is loaded and then set property fontLoaded to true
+  if (!fontLoaded) {
+    return (
+      <AppLoading
+        startAsync={fetchFonts}
+        onFinish={() => setFontLoaded(true)}
+        onError={(err) => console.log(err)}
+      />
+    );
+  }
+
   return (
     <View>
       <Text>Open up App.js to start working on your app!</Text>
