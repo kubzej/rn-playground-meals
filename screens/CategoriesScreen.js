03221c53470987bd6ab3b91d0d1998140c994fa0
Jakub Mares <jakub.mares@heidelbergcement.com>
2022-02-22 18:18:55 +0100

    configuring header with navigation options as property of CategoriesScreen

--- screens/CategoriesScreen.js
+++ screens/CategoriesScreen.js
@@ -6,9 +6,11 @@ import {
   Button,
   FlatList,
   TouchableOpacity,
+  Platform,
 } from 'react-native';
 
 import { CATEGORIES } from '../data/dummy-data';
+import Colors from '../constants/Colors';
 
 const CategoriesScreen = (props) => {
   // it will receive item data and return react component
@@ -30,6 +32,15 @@ const CategoriesScreen = (props) => {
   return (
     <FlatList data={CATEGORIES} renderItem={renderGridItem} numColumns={2} />
   );
+};
+
+// configuring header with navigation options as property of CategoriesScreen
+CategoriesScreen.navigationOptions = {
+  headerTitle: 'Meal Categories',
+  headerStyle: {
+    backgroundColor: Platform.OS === 'android' ? Colors.primaryColor : 'WHITE',
+  },
+  headerTintColor: Platform.OS === 'android' ? 'white' : Colors.primaryColor, // color of text
 };
 
 const styles = StyleSheet.create({
