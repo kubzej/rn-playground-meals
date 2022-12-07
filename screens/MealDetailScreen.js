86bc50cbb01ee650e49a70da121b23bada9421ff
Jakub Mares <jakub.mares@heidelbergcement.com>
2022-12-07 22:22:30 +0100

    provide data to meal detail screen

--- screens/MealDetailScreen.js
+++ screens/MealDetailScreen.js
@@ -1,8 +1,34 @@
-import { Text } from 'react-native';
+import { Text, View, Image } from 'react-native';
+import MealDetails from '../components/mealDetails';
+
+import { MEALS } from '../data/dummy-data';
 
 function MealDetailScreen({ route }) {
   const mealId = route.params.mealId;
-  return <Text>Meal Detail Screen {mealId}</Text>;
+
+  // find meal which meal.id === mealId I get from route navigation
+  const selectedMeal = MEALS.find((meal) => meal.id === mealId);
+  return (
+    <View>
+      <Image source={{ uri: selectedMeal.imageUrl }} />
+      <Text>{selectedMeal.title}</Text>
+      <View>
+        <MealDetails
+          duration={selectedMeal.duration}
+          complexity={selectedMeal.complexity}
+          affordability={selectedMeal.affordability}
+        />
+      </View>
+      <Text>Ingredients</Text>
+      {selectedMeal.ingredients.map((ingredient) => (
+        <Text key={ingredient}>{ingredient}</Text>
+      ))}
+      <Text>Steps</Text>
+      {selectedMeal.steps.map((step) => (
+        <Text key={step}>{step}</Text>
+      ))}
+    </View>
+  );
 }
 
 export default MealDetailScreen;
