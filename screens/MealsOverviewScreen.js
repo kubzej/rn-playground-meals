7837b91b749d849243e777ee01dfdba6d548f8ac
Jakub Mares <jakub.mares@heidelbergcement.com>
2022-12-07 20:58:44 +0100

    render meal titles on each meal overview screen

--- screens/MealsOverviewScreen.js
+++ screens/MealsOverviewScreen.js
@@ -1,4 +1,5 @@
-import { View, Text, StyleSheet } from 'react-native';
+import { View, FlatList, StyleSheet } from 'react-native';
+import MealItem from '../components/MealItem';
 
 import { MEALS } from '../data/dummy-data';
 
@@ -6,9 +7,22 @@ function MealsOverviewScreen({ route }) {
   // sending this param categoryId from CategoriesScreen so I can use it here
   const catId = route.params.categoryId;
 
+  // get all meals that belong to category, return true if meals has a categoryId in its array
+  const displayedMeals = MEALS.filter((mealItem) => {
+    return mealItem.categoryIds.indexOf(catId) >= 0;
+  });
+
+  function renderMealItem(itemData) {
+    return <MealItem title={itemData.item.title} />;
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Meals Overview Screen - {catId}</Text>
+      <FlatList
+        data={displayedMeals}
+        keyExtractor={(item) => item.id}
+        renderItem={renderMealItem}
+      />
     </View>
   );
 }
