6f0a2fd5ba690c7f521bf3b3dbcff3d478682952
Jakub Mares <jakub.mares@heidelbergcement.com>
2022-12-08 14:14:42 +0100

    set add and remove favorites functions

--- store/context/favorites-context.js
+++ store/context/favorites-context.js
@@ -7,7 +7,30 @@ export const FavoritesContext = createContext({
 });
 
 function FavoritesContextProvider({ children }) {
-  return <FavoritesContext.Provider>{children}</FavoritesContext.Provider>;
+  const [favoriteMealsIds, setFavoriteMealsIds] = useState([]);
+
+  function addFavoriteFunc(id) {
+    // creates list of favIds and add id param at the end
+    setFavoriteMealsIds((currentFavdIds) => [...currentFavdIds, id]);
+  }
+  function removeFavoriteFunc(id) {
+    setFavoriteMealsIds((currentFavdIds) =>
+      // filter out mealId which we got as param
+      currentFavdIds.filter((mealId) => mealId !== id)
+    );
+  }
+
+  const value = {
+    ids: favoriteMealsIds,
+    addFavorite: addFavoriteFunc,
+    removeFavorite: removeFavoriteFunc,
+  };
+
+  return (
+    <FavoritesContext.Provider value={value}>
+      {children}
+    </FavoritesContext.Provider>
+  );
 }
 
 export default FavoritesContextProvider;
